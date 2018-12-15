646c2c9516067d7a741b8500175d8c899346e07f
René Bühling <rene.buehling@gmx.de>
2018-12-15 19:15:11 +0100

    + cs Project, (c) 2018

--- runtimeDLL/src/Utils/Audio.cs
+++ runtimeDLL/src/Utils/Audio.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 /// License:
 /// The MIT License (MIT)
 /// 
-/// Copyright (c) 2017 René Bühling, www.gamedev-profi.de
+/// Copyright (c) 2018 René Bühling, www.gamedev-profi.de
 /// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
